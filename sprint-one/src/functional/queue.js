7e272aa0f9118272ca19df7ab50c513f4afae90b
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-19 15:56:30 -0700

    Completed - Functional Queue & Stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,22 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var newKey = String(someInstance.size()); // '0'
+    storage[newKey] = value;
+    return someInstance.size();
   };
 
   someInstance.dequeue = function() {
+    var element = storage['0'];
+    for (var i = 0; i < (someInstance.size() - 1); i++) {
+      storage[String(i)] = storage[String(i + 1)];
+    }
+    delete storage[String(someInstance.size() - 1)];
+    return element;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,13 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var size = String(someInstance.size());
+    storage[size] = value;
+    return someInstance.size();
   };
 
   someInstance.pop = function() {
+    var lastIdx = String(someInstance.size() - 1);
+    var element = storage[lastIdx];
+    delete storage[lastIdx];
+    return element;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+// [1, 2, 3].push(4) returns 4
