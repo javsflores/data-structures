49f8a6560c61b85d7d121e6b95c2a698d49299f8
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-20 10:37:01 -0700

    Completed Functional Shared - Passed all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    var newIndex = String(this.size());
+    this.storage[newIndex] = value;
+    return this.size();
+  },
+  dequeue: function() {
+    var firstValue = this.storage['0'];
+    for (var i = 0; i < this.size() - 1; i++) {
+      this.storage[String(i)] = this.storage[String(i + 1)];
+    }
+    delete this.storage[String(i)];
+    return firstValue;
+  },
+  size: function () {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  pop: function() {
+    var lastIndex = String(this.size() - 1);
+    var lastValue = this.storage[lastIndex];
+    delete this.storage[lastIndex];
+    return lastValue;
+  },
+  size: function () {
+    return Object.keys(this.storage).length;
+  },
+  push: function(value) {
+    var newIndex = String(this.size());
+    this.storage[newIndex] = value;
+    return this.size();
+  }
+};
+
+// var x = Stack();
+// x.push(1);
+// x.pop();
+// var y = Stack();
 
 
+
+// var Car = function(loc) {
+//   var obj = {};
+//   _.extend(obj, stackMethods);
+//   obj.loc = loc;
+//   return obj;
+// };
+// var CarMethods = {
+//   drive: function() {}
+// };
