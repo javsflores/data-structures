eb549196d8e16b1c9c4447b58ce9c0af98e1997b
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-21 11:27:55 -0700

    Completed linkedList - All tests pass

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,58 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  /*
+  list: {
+    head: node1,
+    tail: node3
+  }
+
+  node1 = {
+    value: 0,
+    next: node2
+  }
+  node2 = {
+    value: 23,
+    next: node3
+  }
+  node3 = {
+    value: 4,
+    next: null
+  }
+  */
+
   list.addToTail = function(value) {
+    // add node to the end of list
+    // point list.tail at that node
+    // if this node is the first node in the list,
+    //   point list.head at that node too
+    var newNode = Node(value);
+    if (list.tail) {
+      list.tail.next = newNode;
+    }
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var headVal = list.head.value;
+    list.head = list.head.next;
+    return headVal;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
