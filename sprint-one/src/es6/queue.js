21bfbfa3ddef93d7b00c2bccecc336211c25fe93
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-20 17:01:12 -0700

    Completed es6 - Sprint 1 Complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  enqueue(value) {
+    var newIndex = String(this.size());
+    this.storage[newIndex] = value;
+    return this.size();
+  }
+
+  dequeue() {
+    var firstValue = this.storage['0'];
+    for (var i = 0; i < this.size() - 1; i++) {
+      this.storage[String(i)] = this.storage[String(i + 1)];
+    }
+    delete this.storage[String(i)];
+    return firstValue;
   }
 
 }
+
+
+var x = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  push(value) {
+    var lastIndex = String(this.size());
+    this.storage[lastIndex] = value;
+    return this.size();
+  }
+
+  pop() {
+    var lastIndex = String(this.size() - 1);
+    var lastValue = this.storage[lastIndex];
+    delete this.storage[lastIndex];
+    return lastValue;
+  }
 }
