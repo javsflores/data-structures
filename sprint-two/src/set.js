c3db74e09fc559476e76b93b48259f565f4582cc
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-22 10:13:50 -0700

    Completed set.js - Set.js all tests pass.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (_.indexOf(this._storage, item) > -1) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var index = _.indexOf(this._storage, item);
+
+  if (index !== -1) {
+    this._storage.splice(index, 1);
+  }
 };
 
 /*
