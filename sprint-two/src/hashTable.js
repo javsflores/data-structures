243172ff07e0eb6a2f2d427174a86ba78294bf26
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-22 12:04:56 -0700

    Completed hashTable.js - All tests pass.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,39 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  for (var i = 0; i < this._limit; i++) {
+    this._storage.set(i, []);
+  }
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this.remove(k);
+  var pair = [k, v];
+  var bucket = this._storage.get(index);
+  bucket.push(pair);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index).slice();
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
