1a7815189e99be1a75afb4983cac2138d823ecf9
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-22 15:50:39 -0700

    Completed binarySearchTree.js - Tests all pass

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,51 @@
 var BinarySearchTree = function(value) {
+  var BST = Object.create(BSTMethods);
+  BST.value = value;
+  BST.left = null;
+  BST.right = null;
+  return BST;
 };
 
+BSTMethods = {};
+
+BSTMethods.insert = function(value) {
+  var newBST = BinarySearchTree(value);
+  if (value > this.value) { // right
+    if (!this.right) {
+      this.right = newBST;
+    } else {
+      this.right.insert(value);
+    }
+  } else if (value < this.value) { // left
+    if (!this.left) {
+      this.left = newBST;
+    } else {
+      this.left.insert(value);
+    }
+  }
+};
+
+BSTMethods.contains = function(value) {
+  if (this.value === value ) {
+    return true;
+  } else if (this.value > value && this.left) {
+    return this.left.contains(value);
+  } else if (this.value < value && this.right) {
+    return this.right.contains(value);
+  } else {
+    return false;
+  }
+};
+
+BSTMethods.depthFirstLog = function(callback) {
+  this.value = callback(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(callback);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
